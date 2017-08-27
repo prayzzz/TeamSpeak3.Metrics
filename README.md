9aa515ef1c1670d570b553d2f5fe02ef7aeb72a7
prayzzz <prayzzz@outlook.com>
2017-08-27 20:19:42 +0200

    - updated settings - added ops stuff

---
+++ README.md
@@ -0,0 +1,14 @@
+# odd-monitor
+
+## Requirements
+
+* .NET Core 2.0 SDK
+
+## Dev Setup
+
+* set environment variables
+
+```
+TeamSpeak3Metrics_App__TeamSpeak__QueryUsername=
+TeamSpeak3Metrics_App__TeamSpeak__QueryPassword=
+```
--- src/TeamSpeak3.Metrics/Program.cs
+++ src/TeamSpeak3.Metrics/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -10,9 +11,9 @@ namespace TeamSpeak3.Metrics
     {
         public static void Main(string[] args)
         {
-            var hostConfig = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .Build();
+            var hostConfig = new ConfigurationBuilder().AddInMemoryCollection(new Dictionary<string, string> { { "urls", "http://localhost:5002" } })
+                                                       .AddCommandLine(args)
+                                                       .Build();
 
             new WebHostBuilder().UseKestrel()
                                 .UseConfiguration(hostConfig)
@@ -29,7 +30,7 @@ namespace TeamSpeak3.Metrics
             var env = context.HostingEnvironment;
             builder.AddJsonFile("appsettings.json", true, true)
                    .AddJsonFile($"appsettings.{env.EnvironmentName.ToLower()}.json", true, true);
-            builder.AddEnvironmentVariables();
+            builder.AddEnvironmentVariables("teamspeak3metrics_");
         }
 
         private static void ConfigureLogging(WebHostBuilderContext context, ILoggingBuilder builder)
