11ded0c91c4dda450a7a092eab10ee4a41ce57da
prayzzz <prayzzz@outlook.com>
2017-10-08 16:15:09 +0200

    - added wait time to tests

--- test/TeamSpeak3.Metrics.Test/Query/TeamspeakConnectionTest.cs
+++ test/TeamSpeak3.Metrics.Test/Query/TeamspeakConnectionTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using prayzzz.Common.Unit;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using TeamSpeak3.Metrics.Query;
 
@@ -13,52 +14,55 @@ namespace TeamSpeak3.Metrics.Test.Query
         public async Task TestClientList()
         {
             using (var server = new TelnetServer())
-            using (var connection = new TeamSpeakConnection(new ConsoleLogger<TeamSpeakConnection>()))
             {
-                var isConnected = await connection.Connect(server.Ip, server.Port);
-                if (!isConnected)
+                Thread.Sleep(100);
+                using (var connection = new TeamSpeakConnection(new ConsoleLogger<TeamSpeakConnection>()))
                 {
-                    Assert.Fail("Missing connection");
+                    var isConnected = await connection.Connect(server.Ip, server.Port);
+                    if (!isConnected)
+                    {
+                        Assert.Fail("Missing connection");
+                    }
+
+                    var user1 = new Dictionary<string, string>
+                    {
+                        { "clid", "3" },
+                        { "cid", "4" },
+                        { "client_nickname", "prayzzz\\s\\p\\sPatrick" },
+                        { "client_type", "0" }
+                    };
+
+                    var user2 = new Dictionary<string, string>
+                    {
+                        { "clid", "1" },
+                        { "cid", "5" },
+                        { "client_nickname", "RussianBeeAdmin" },
+                        { "client_type", "1" }
+                    };
+
+                    server.Setup("clientlist",
+                                 Translator.Translate(new List<Dictionary<string, string>> { user1, user2 }) +
+                                 Response.NewLine +
+                                 "error id=0 msg=ok");
+
+                    var result = await connection.ClientList();
+
+                    Assert.AreEqual(0, result.ErrorId);
+                    Assert.AreEqual("ok", result.ErrorMessage);
+                    Assert.AreEqual(2, result.Data.Count);
+
+                    var client1 = result.Data[0];
+                    Assert.AreEqual(4, client1.Cid);
+                    Assert.AreEqual(3, client1.Clid);
+                    Assert.AreEqual("prayzzz | Patrick", client1.ClientNickname);
+                    Assert.AreEqual(0, client1.ClientType);
+
+                    var client2 = result.Data[1];
+                    Assert.AreEqual(5, client2.Cid);
+                    Assert.AreEqual(1, client2.Clid);
+                    Assert.AreEqual("RussianBeeAdmin", client2.ClientNickname);
+                    Assert.AreEqual(1, client2.ClientType);
                 }
-
-                var user1 = new Dictionary<string, string>
-                {
-                    { "clid", "3" },
-                    { "cid", "4" },
-                    { "client_nickname", "prayzzz\\s\\p\\sPatrick" },
-                    { "client_type", "0" }
-                };
-
-                var user2 = new Dictionary<string, string>
-                {
-                    { "clid", "1" },
-                    { "cid", "5" },
-                    { "client_nickname", "RussianBeeAdmin" },
-                    { "client_type", "1" }
-                };
-
-                server.Setup("clientlist",
-                             Translator.Translate(new List<Dictionary<string, string>> { user1, user2 }) +
-                             Response.NewLine +
-                             "error id=0 msg=ok");
-
-                var result = await connection.ClientList();
-
-                Assert.AreEqual(0, result.ErrorId);
-                Assert.AreEqual("ok", result.ErrorMessage);
-                Assert.AreEqual(2, result.Data.Count);
-
-                var client1 = result.Data[0];
-                Assert.AreEqual(4, client1.Cid);
-                Assert.AreEqual(3, client1.Clid);
-                Assert.AreEqual("prayzzz | Patrick", client1.ClientNickname);
-                Assert.AreEqual(0, client1.ClientType);
-
-                var client2 = result.Data[1];
-                Assert.AreEqual(5, client2.Cid);
-                Assert.AreEqual(1, client2.Clid);
-                Assert.AreEqual("RussianBeeAdmin", client2.ClientNickname);
-                Assert.AreEqual(1, client2.ClientType);
             }
         }
 
@@ -66,11 +70,14 @@ namespace TeamSpeak3.Metrics.Test.Query
         public async Task TestConnect()
         {
             using (var server = new TelnetServer())
-            using (var connection = new TeamSpeakConnection(new ConsoleLogger<TeamSpeakConnection>()))
             {
-                var isConnected = await connection.Connect(server.Ip, server.Port);
+                Thread.Sleep(100);
+                using (var connection = new TeamSpeakConnection(new ConsoleLogger<TeamSpeakConnection>()))
+                {
+                    var isConnected = await connection.Connect(server.Ip, server.Port);
 
-                Assert.IsTrue(isConnected);
+                    Assert.IsTrue(isConnected);
+                }
             }
         }
 
@@ -78,20 +85,23 @@ namespace TeamSpeak3.Metrics.Test.Query
         public async Task TestLogin()
         {
             using (var server = new TelnetServer())
-            using (var connection = new TeamSpeakConnection(new ConsoleLogger<TeamSpeakConnection>()))
             {
-                var isConnected = await connection.Connect(server.Ip, server.Port);
-                if (!isConnected)
+                Thread.Sleep(100);
+                using (var connection = new TeamSpeakConnection(new ConsoleLogger<TeamSpeakConnection>()))
                 {
-                    Assert.Fail("Missing connection");
+                    var isConnected = await connection.Connect(server.Ip, server.Port);
+                    if (!isConnected)
+                    {
+                        Assert.Fail("Missing connection");
+                    }
+
+                    server.Setup("login user pw", "error id=0 msg=ok");
+
+                    var result = await connection.Login("user", "pw");
+
+                    Assert.AreEqual(0, result.ErrorId);
+                    Assert.AreEqual("ok", result.ErrorMessage);
                 }
-
-                server.Setup("login user pw", "error id=0 msg=ok");
-
-                var result = await connection.Login("user", "pw");
-
-                Assert.AreEqual(0, result.ErrorId);
-                Assert.AreEqual("ok", result.ErrorMessage);
             }
         }
 
@@ -99,19 +109,22 @@ namespace TeamSpeak3.Metrics.Test.Query
         public async Task TestUse()
         {
             using (var server = new TelnetServer())
-            using (var connection = new TeamSpeakConnection(new ConsoleLogger<TeamSpeakConnection>()))
             {
-                var isConnected = await connection.Connect(server.Ip, server.Port);
-                if (!isConnected)
+                Thread.Sleep(100);
+                using (var connection = new TeamSpeakConnection(new ConsoleLogger<TeamSpeakConnection>()))
                 {
-                    Assert.Fail("Missing connection");
+                    var isConnected = await connection.Connect(server.Ip, server.Port);
+                    if (!isConnected)
+                    {
+                        Assert.Fail("Missing connection");
+                    }
+
+                    server.Setup("use port=9987", "error id=0 msg=ok");
+                    var result = await connection.Use(9987);
+
+                    Assert.AreEqual(0, result.ErrorId);
+                    Assert.AreEqual("ok", result.ErrorMessage);
                 }
-
-                server.Setup("use port=9987", "error id=0 msg=ok");
-                var result = await connection.Use(9987);
-
-                Assert.AreEqual(0, result.ErrorId);
-                Assert.AreEqual("ok", result.ErrorMessage);
             }
         }
     }
